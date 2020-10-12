e1d53cae1eb25fc282cc74a1ff57f3924136fb9e
KRZYSZTOFP1989 <62620232+KRZYSZTOFP1989@users.noreply.github.com>
2020-10-12 17:04:11 +0200

    Cc mail Empty info

---
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -0,0 +1,17 @@
+package com.crud.tasks.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Mail {
+
+    private String mailTo;
+    private String toCc;
+    private String subject;
+    private String message;
+
+}
